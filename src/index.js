16c4c3ebcf63b741f08f9efac8fb598455c63862
William Johnson <catfishfishcat10101@gmail.com>
2024-12-30 14:54:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.clasList.toggle('show');
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
